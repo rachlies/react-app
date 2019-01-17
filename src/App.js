a988d9b464e7d1182cc89c2bae1229a9a909be6f
= <roshan.kumar417@gmail.com>
2019-01-17 04:10:02 +0100

    using setState to update state value

--- src/App.js
+++ src/App.js
@@ -12,13 +12,24 @@ class App extends Component {
     ]
   };
 
+  switchButtonHandler = () => {
+    // console.log("hellow!");
+    this.setState({
+      person : [
+        {Name : "Priya", Age : 23},
+        {Name : "Roshan", Age : 25}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React app</h1>
         <p>It's working!!</p>
-          <Person Name = {this.state.person[0].Name} Age = {this.state.person[0].Age}/>
-          <Person Name = {this.state.person[1].Name} Age = {this.state.person[1].Age}>Some text</Person>
+        <button onClick = {this.switchButtonHandler}>Switch Names</button>
+        <Person Name = {this.state.person[0].Name} Age = {this.state.person[0].Age}/>
+        <Person Name = {this.state.person[1].Name} Age = {this.state.person[1].Age}>Some text</Person>
       </div>
     );
   }
