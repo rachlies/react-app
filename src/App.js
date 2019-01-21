94bfb1df0186133d60551b5f140c822d3ef9c6ec
= <roshan.kumar417@gmail.com>
2019-01-21 12:19:10 +0100

    using radium module fo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -57,7 +58,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      },
     }
 
     let person = null;
@@ -79,6 +84,10 @@ class App extends Component {
         </div>
       )
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
       buttonMsg = "Hide";
     }
     let classes = [];
@@ -104,4 +113,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
