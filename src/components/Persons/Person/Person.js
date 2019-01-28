753314bbbf3d2b70491e1bac3b7dd483ef8e9583
= <roshan.kumar417@gmail.com>
2019-01-28 01:21:09 +0100

    converted person.js and persons.js into stateful

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
 
-    return (
-        <div className = {classes.Person}>
-            <p onClick = {props.click}>I'm  {props.Name}, {props.Age} years old Person!!</p>
-            <input onChange = {props.changed}></input>
-        </div>    
-    )
-};
+class Person extends Component {
+    render() {
+        return (
+            <div className = {classes.Person}>
+                <p onClick = {this.props.click}>I'm  {this.props.Name}, {this.props.Age} years old Person!!</p>
+                <input onChange = {this.props.changed}></input>
+            </div>    
+        )
+    }
+}
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 
-const Persons = (props) => props.persons.map((p,idx) => {
-    return <Person 
-            click = {() => props.clicked(idx)}
-            Name = {p.Name}
-            Age = {p.Age}
-            key = {p.id}
-            changed = {(event) => props.changed(event, p.id)}/>
-  })
+class Persons extends Component {
+    render () {
+        return this.props.persons.map((p,idx) => {
+            return <Person 
+                    click = {() => this.props.clicked(idx)}
+                    Name = {p.Name}
+                    Age = {p.Age}
+                    key = {p.id}
+                    changed = {(event) => this.props.changed(event, p.id)}/>
+          })
+    }
+}
 
-  export default Persons;
+export default Persons;
